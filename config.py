6a3eecd4cdb9bc52c2a40c86ec338dd02d418f4e
ElDunkin <123667829+ElDunkin@users.noreply.github.com>
2025-12-16 00:45:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flaskext.mysql import MySQL
+import os
 
 mysql = MySQL()
 
 def conexion_base_de_datos():
     main = Flask(__name__)
-    main.config['MYSQL_DATABASE_USER'] = 'root'
-    main.config['MYSQL_DATABASE_DB'] = 'petfriendly_db' 
-    main.config['MYSQL_DATABASE_PASSWORD'] = ''
-    main.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+    main.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST')
+    main.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USER')
+    main.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('DB_PASSWORD')
+    main.config['MYSQL_DATABASE_DB'] = os.getenv('DB_NAME')
+    main.config['MYSQL_DATABASE_PORT'] = int(os.getenv('DB_PORT'))
 
     mysql.init_app(main)
-    
     return main
