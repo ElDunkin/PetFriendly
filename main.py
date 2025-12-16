40ed4d71e4837196c94d6f3da7e585c5d26d6199
ElDunkin <123667829+ElDunkin@users.noreply.github.com>
2025-12-16 00:28:29 -0500

    Refactor app initialization and blueprint registration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from config import conexion_base_de_datos
 from controllers.rutas_principales import rutas_principales
 from controllers.rutas_usuarios import rutas_usuarios
@@ -20,43 +21,37 @@ from controllers.rutas_jornada import rutas_jornada
 from controllers.rutas_adopcion import rutas_adopciones
 from controllers.rutas_procedimientos import rutas_procedimientos
 
+load_dotenv()
 
-
-
-main = conexion_base_de_datos()
-main.secret_key = os.urandom(24)
-
-
+app = conexion_base_de_datos()
+app.secret_key = os.urandom(24)
 
 UPLOAD_FOLDER = 'contratos'
 ALLOWED_EXTENSIONS = {'pdf'}
 MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB
 
-main.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-main.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 
-
-main.register_blueprint(rutas_principales)
-main.register_blueprint(rutas_usuarios)
-main.register_blueprint(rutas_pacientes)
-main.register_blueprint(rutas_login)
-main.register_blueprint(rutas_dashboard)
-main.register_blueprint(rutas_consultas)
-main.register_blueprint(rutas_recuperar_contraseña)
-main.register_blueprint(rutas_insumos)
-main.register_blueprint(rutas_medicamentos)
-main.register_blueprint(rutas_rescatados)
-main.register_blueprint(rutas_donacion)
-main.register_blueprint(rutas_permanencia)
-main.register_blueprint(rutas_salidas)
-main.register_blueprint(rutas_alimentos)
-main.register_blueprint(rutas_citas)
-main.register_blueprint(rutas_carne_vacunas)
-main.register_blueprint(rutas_jornada)
-main.register_blueprint(rutas_adopciones)
-main.register_blueprint(rutas_procedimientos)
+app.register_blueprint(rutas_principales)
+app.register_blueprint(rutas_usuarios)
+app.register_blueprint(rutas_pacientes)
+app.register_blueprint(rutas_login)
+app.register_blueprint(rutas_dashboard)
+app.register_blueprint(rutas_consultas)
+app.register_blueprint(rutas_recuperar_contraseña)
+app.register_blueprint(rutas_insumos)
+app.register_blueprint(rutas_medicamentos)
+app.register_blueprint(rutas_rescatados)
+app.register_blueprint(rutas_donacion)
+app.register_blueprint(rutas_permanencia)
+app.register_blueprint(rutas_salidas)
+app.register_blueprint(rutas_alimentos)
+app.register_blueprint(rutas_citas)
+app.register_blueprint(rutas_carne_vacunas)
+app.register_blueprint(rutas_jornada)
+app.register_blueprint(rutas_adopciones)
+app.register_blueprint(rutas_procedimientos)
 
 if __name__ == '__main__':
-    main.run(debug=True)
-    
-
+    app.run(debug=True)
